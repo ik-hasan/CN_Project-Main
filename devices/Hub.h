bdf9466aee33d35d289fd73df84e45d22bf60b22
ik-hasan <ikramul7388@gmail.comm>
2026-06-23 12:52:16 +0530

    difining hub

---
+++ devices/Hub.h
@@ -0,0 +1,34 @@
+// #ifndef HUB_H
+// #define HUB_H
+
+// #include "Device.h"
+
+// class Hub : public Device {
+// public:
+//     Hub(string id);
+
+//     // hub receives and broadcasts
+//     void receive(const string& data, Device* sender) override;
+// };
+
+// #endif
+
+
+
+
+//Commented above code in step 4
+#ifndef HUB_H
+#define HUB_H
+
+#include "Device.h"
+
+class Hub : public Device {
+
+public:
+    Hub(string id);
+    // UPDATED to use Frame
+    void receive(Frame frame, Device* sender) override;
+
+};
+
+#endif
